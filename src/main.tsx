c4897bc8255f14ff5486c199e2f8bff1b1690686
Jokhid <hidalgo.j.carlos@gmail.com>
2026-06-22 14:54:24 +0200

    Load safe PDF previsor box fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import './footer-fix.css';
 import './executive-summary-format.ts';
+import './pdf-previsor-box-fix.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
