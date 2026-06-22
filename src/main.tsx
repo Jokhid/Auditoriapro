c4d42283a0c15059dd40b2d048e57150cb071ae6
Jokhid <hidalgo.j.carlos@gmail.com>
2026-06-22 15:00:24 +0200

    Use literal PDF download generator

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import './footer-fix.css';
 import './executive-summary-format.ts';
-import './pdf-previsor-box-fix.ts';
+import './literal-pdf-download.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
